067cb8faf5f12b1ebe1e5e29ac3d64add0d51540
Claire <a0505kimo@hotmail>
2020-10-06 15:32:34 +0800

    新增MaterialActivity，活動選擇BasicActivity

--- app/src/main/java/com/claire/atm/LoginActivity.kt
+++ app/src/main/java/com/claire/atm/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.claire.atm
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +15,24 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun login(view: View) {
+        val userid = ed_userid.text.toString()
+        val passwd = ed_passwd.text.toString()
+        
+        if (userid == "claire" && passwd == "1234"){
+            Toast.makeText(this, "登入成功", Toast.LENGTH_SHORT).show()
+
+            //將帳號與密碼放入 Intent 物件中，再呼叫 Activity 的 setResult 方法，
+            intent.putExtra("LOGIN_USERID", userid)
+            intent.putExtra("LOGIN_PASSWD", passwd)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        } else{
+            AlertDialog.Builder(this)
+                .setTitle("Atm")
+                .setMessage("登入失敗")
+                .setPositiveButton("ok", null)
+                .show()
+        }
 
     }
 
--- app/src/main/java/com/claire/atm/MainActivity.kt
+++ app/src/main/java/com/claire/atm/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.claire.atm
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    //登入狀態
+    //定義功能常數
+    companion object {
+        val RC_LOGIN = 30
+    }
+    //一開始未登入狀態
     var login = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +19,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //檢查使用者是否登入過
         if (!login){
+            Intent(this, LoginActivity:: class.java).apply {
+                //startActivityForResult 是到另一個畫面中並取得結果 ( 資料 ) 的
+                startActivityForResult(this, RC_LOGIN)
+            }
+
+        }
+    }
+
+    //傳回LoginActivity Intent過來的資料
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //先判斷是否是 requestCode 是否為 RC_LOGIN，
+        // 是的話再 進一步判斷 resultCode 是否為 RESULT_OK，最後再取得 Intent 中的資料
+        if (requestCode == RC_LOGIN){
+            if (resultCode == Activity.RESULT_OK){
+                val userid = data?.getStringExtra("LOGIN_USERID")
+                val passwd = data?.getStringExtra("LOGIN_PASSWD")
+                Log.d("RESULT", "$userid / $passwd")
+            }
 
         }
     }
